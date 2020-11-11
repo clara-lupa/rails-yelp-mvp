ec5f8dec49665ff329bb73bb210ae8d40f663796
clara wolff <cwo@systemli.org>
2020-11-11 13:00:47 +0100

    completed backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+puts 'Generating 5 restaurants'
+5.times do
+  r = Restaurant.new
+  r.name = Faker::Restaurant.name
+  r.address = Faker::Address.full_address
+  r.phone_number = Faker::PhoneNumber.phone_number
+  r.category = Restaurant::CATEGORIES.sample
+  r.save
+end
+
+puts 'Generated 5 restaurants'
